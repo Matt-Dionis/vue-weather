c12282733e98f206fe506006231808fbfa050825
Matt Dionis <mattdionis@MattBook-Pro.local>
2017-07-04 13:26:05 -0400

    Fix favorite routing

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <h2 class="md-title" style="flex: 1">Vue Weather</h2>
       <md-button
         v-if="favoriteLocation"
-        @click.native="$router.push({ name: 'forecast', params: { id: favoriteLocation.id }})">
+        @click.native="$router.push({ name: 'place', params: { id: favoriteLocation.id }})">
         <md-icon>star</md-icon> {{favoriteLocation.name}}
       </md-button>
     </md-toolbar>
